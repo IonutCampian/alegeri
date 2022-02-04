56f9eacb0d29aea7344419dea93fed5b5099818e
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2022-02-04 16:42:08 +0200

    Update CandidatesRepository.java

--- src/main/java/logReg/Repository/CandidatesRepository.java
+++ src/main/java/logReg/Repository/CandidatesRepository.java
@@ -4,4 +4,5 @@ import logReg.model.Candidates;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidatesRepository extends JpaRepository<Candidates, Long> {
+   <Optional> Candidates findByUsername(String username);
 }
